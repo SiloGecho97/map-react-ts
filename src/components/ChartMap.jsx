7ec6f01a8f93189444a612610d528abc7221d944
Silas Getachew <silogecho@gmail.com>
2022-03-03 09:17:53 +0300

    fix: small issue

--- src/components/ChartMap.jsx
+++ src/components/ChartMap.jsx
@@ -131,9 +131,9 @@ arrowSeries.bullets.push(function() {
 
 let cities = [
   {
-    id: "london",
-    title: "London",
-    geometry: { type: "Point", coordinates: [-0.1262, 51.5002] },
+    id: "Addis",
+    title: "Addis Ababa",
+    geometry: { type: "Point", coordinates: [8.9806, 38.7578] },
   },
   {
     id: "brussels",
@@ -143,67 +143,7 @@ let cities = [
     id: "prague",
     title: "Prague",
     geometry: { type: "Point", coordinates: [14.4205, 50.0878] }
-  }, {
-    id: "athens",
-    title: "Athens",
-    geometry: { type: "Point", coordinates: [23.7166, 37.9792] }
-  }, {
-    id: "reykjavik",
-    title: "Reykjavik",
-    geometry: { type: "Point", coordinates: [-21.8952, 64.1353] }
-  }, {
-    id: "dublin",
-    title: "Dublin",
-    geometry: { type: "Point", coordinates: [-6.2675, 53.3441] }
-  }, {
-    id: "oslo",
-    title: "Oslo",
-    geometry: { type: "Point", coordinates: [10.7387, 59.9138] }
-  }, {
-    id: "lisbon",
-    title: "Lisbon",
-    geometry: { type: "Point", coordinates: [-9.1355, 38.7072] }
-  }, {
-    id: "moscow",
-    title: "Moscow",
-    geometry: { type: "Point", coordinates: [37.6176, 55.7558] }
-  }, {
-    id: "belgrade",
-    title: "Belgrade",
-    geometry: { type: "Point", coordinates: [20.4781, 44.8048] }
-  }, {
-    id: "bratislava",
-    title: "Bratislava",
-    geometry: { type: "Point", coordinates: [17.1547, 48.2116] }
-  }, {
-    id: "ljublana",
-    title: "Ljubljana",
-    geometry: { type: "Point", coordinates: [14.5060, 46.0514] }
-  }, {
-    id: "madrid",
-    title: "Madrid",
-    geometry: { type: "Point", coordinates: [-3.7033, 40.4167] }
-  }, {
-    id: "stockholm",
-    title: "Stockholm",
-    geometry: { type: "Point", coordinates: [18.0645, 59.3328] }
-  }, {
-    id: "bern",
-    title: "Bern",
-    geometry: { type: "Point", coordinates: [7.4481, 46.9480] }
-  }, {
-    id: "kiev",
-    title: "Kiev",
-    geometry: { type: "Point", coordinates: [30.5367, 50.4422] }
-  }, {
-    id: "paris",
-    title: "Paris",
-    geometry: { type: "Point", coordinates: [2.3510, 48.8567] }
-  }, {
-    id: "new york",
-    title: "New York",
-    geometry: { type: "Point", coordinates: [-74, 40.43] }
-  }];
+  },];
 
 citySeries.data.setAll(cities);
 
