1de63f039321582086a3b3a6b00159ef8a4094db
Silas Getachew <silogecho@gmail.com>
2022-03-03 11:18:16 +0300

    feat: add leaflet map

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,12 @@ import ChartMap from './components/ChartMap';
 import GoogleMap from './components/GoogleMap/GoogleMap';
 import SimpleMap from './components/GoogleMap/SimpleMap';
 import WorldMapAtlas from './components/GoogleMap/WorldMapAtlas';
+import LeafletMap from './components/LeafletMap';
 
 function App() {
   return (
     <div className="App">
+      <LeafletMap/>
       <WorldMapAtlas/>
       {/* <SimpleMap/> */}
       <ChartMap/>
